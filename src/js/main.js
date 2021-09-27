9b23e0241f931c360dc1df62948e5bcf256835f2
artemzhdanov <jubileewinsthere@gmail.com>
2021-09-27 12:22:54 +0300

    contacts + fix

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,66 @@ function init() {
     let tabNavs = document.querySelectorAll('.tab_navs > *');
     let tabContent = document.querySelectorAll('.tab_content > *');
     let moreButtons = document.querySelectorAll('.more');
+    let modalTriggers = document.querySelectorAll('[data-modal]');
+    let closeModalButton = document.querySelectorAll('.close_modal');
+    let fileInputs = document.querySelectorAll('input[type="file"]');
+    let activeModalElem = null;
+
+
+    if(fileInputs.length) {
+        const getFileNameWithExt = function (event) {
+
+            if (!event || !event.target || !event.target.files || event.target.files.length === 0) {
+                return;
+            }
+
+            const name = event.target.files[0].name;
+            const lastDot = name.lastIndexOf('.');
+
+            const fileName = name.substring(0, lastDot);
+            const ext = name.substring(lastDot + 1);
+
+            const inputId = event.target.id;
+            if(inputId) {
+                const label = document.querySelector(`label[for="${inputId}"]`);
+                label.textContent = fileName+'.'+ext;
+            }
+
+        }
+        Array.from(fileInputs).map((elem,index) => {
+            elem.addEventListener('change', getFileNameWithExt);
+        });
+    }
+
+
+    const closeModal = function () {
+        if(activeModalElem) {
+            document.querySelector('body').classList.remove('modal_open');
+            activeModalElem.style.display = 'none';
+            activeModalElem.style.opacity = '0';
+        }
+    }
+
+    if(closeModalButton.length) {
+        Array.from(closeModalButton).map((elem,index) => {
+            elem.addEventListener('click', closeModal);
+        });
+    }
+
+    if(modalTriggers.length) {
+        const modalHandler = function () {
+            const modalId = this.dataset.modal;
+            activeModalElem = document.getElementById(modalId);
+            if(activeModalElem) {
+                document.querySelector('body').classList.toggle('modal_open');
+                activeModalElem.style.display = 'block';
+                activeModalElem.style.opacity = '1';
+            }
+        }
+        Array.from(modalTriggers).map((elem,index) => {
+            elem.addEventListener('click', modalHandler);
+        });
+    }
 
 
     if(toggleNavButton) {
@@ -24,6 +84,8 @@ function init() {
         const closeAll = function (e) {
             e.preventDefault();
             document.querySelector('body').classList.remove('nav_open');
+            document.querySelector('body').classList.remove('modal_open');
+            closeModal();
         }
         toggleNavButton.addEventListener('click', toggleNavHandler);
         overlay.addEventListener('click', closeAll);
@@ -63,6 +125,20 @@ function init() {
         navs.map((elem,index) => {
             elem.addEventListener('click', tabHandler);
         });
+
+        if(window.location.href.includes('#')) {
+            const tabId = window.location.href.split('#')[1];
+            const matchId = function (elem, i) {
+                if (elem.id == tabId) {
+                    return i;
+                }
+                return false;
+            }
+            let el = navs.find(matchId);
+            if(el) {
+                el.click();
+            }
+        }
     }
 
     if(moreButtons.length) {
